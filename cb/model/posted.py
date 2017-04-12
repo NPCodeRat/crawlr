d5767d2d30606f47c2c951da92c0dbf288e951d8
Nick Poloway <npoloway@catalystdevworks.com>
2017-04-12 10:53:40 -0400

    Applying better practices to query models

--- cb/model/posted.py
+++ cb/model/posted.py
@@ -1,46 +1,33 @@
-from enum import Enum
-
-
-class Posted(Enum):
+class Posted(object):
     """Enum for posting date query param, default to 30 days"""
-    HOURS_24 = "posted=1"
-    DAYS_3 = "posted=3"
-    DAYS_7 = "posted=7"
-    DAYS_30 = None
+    HOURS_24 = dict({'label': '24 hours', 'value': 'posted=1'})
+    DAYS_3 = dict({'label': '3 days', 'value': 'posted=3'})
+    DAYS_7 = dict({'label': '7 days', 'value': 'posted=7'})
+    DAYS_30 = dict({'label': '30 days', 'value': None})
 
-    def __str__(self):
-        if self.value == 'posted=1':
-            return '24 hours'
-        if self.value == 'posted=3':
-            return '3 days'
-        if self.value == 'posted=7':
-            return '7 days'
-        if not self.value:
-            return '30 days'
+    @classmethod
+    def append(cls):
+        return cls.DAYS_30.get('value')
 
-    def __eq__(self, y):
-        return self.value == y.value
-
-    __repr__ = __str__
-
-    @staticmethod
-    def append():
-        return Posted.DAYS_30
-
-    @staticmethod
-    def cmd_append():
+    @classmethod
+    def cmd_append(cls):
         choice = raw_input(
             '*****\nSearch the past...\n\n1 - {}\n2 - {}\n3 - {}\n4 - {}\n\n'
-                .format(Posted.HOURS_24, Posted.DAYS_3,
-                        Posted.DAYS_7, Posted.DAYS_30))
+                .format(cls.HOURS_24.get('label'), cls.DAYS_3.get('label'), cls.DAYS_7.get('label'),
+                        cls.DAYS_30.get('label')))
         if choice == '1':
-            return Posted.HOURS_24
+            print cls.HOURS_24.get('value')
+            return cls.HOURS_24.get('value')
         elif choice == '2':
-            return Posted.DAYS_3
+            print cls.DAYS_3.get('value')
+            return cls.DAYS_3.get('value')
         elif choice == '3':
-            return Posted.DAYS_7
+            print cls.DAYS_7.get('value')
+            return cls.DAYS_7.get('value')
         elif choice == '4':
-            return Posted.DAYS_30
+            print cls.DAYS_30.get('value')
+            return cls.DAYS_30.get('value')
         else:
             print '\nNot an option.  Defaulting to 30 miles.\n'
-            return Posted.DAYS_30
+            print cls.DAYS_30.get('value')
+            return cls.DAYS_30.get('value')
--- cb/model/radius.py
+++ cb/model/radius.py
@@ -1,50 +1,33 @@
-from enum import Enum
-
-
-class Radius(Enum):
+class Radius(object):
     """Enum for distance query param, default to 30 miles"""
-    MILES_5 = 'radius=5'
-    MILES_10 = 'radius=10'
-    MILES_30 = None
-    MILES_50 = 'radius=50'
+    MILES_5 = dict({'label': 'five miles', 'value': 'radius=5'})
+    MILES_10 = dict({'label': 'ten miles', 'value': 'radius=10'})
+    MILES_30 = dict({'label': 'thirty miles', 'value': None})
+    MILES_50 = dict({'label': 'fifty miles', 'value': 'radius=50'})
 
-    def __str__(self):
-        if self.value == 'radius=5':
-            return 'five miles'
-        if self.value == 'radius=10':
-            return 'ten miles'
-        if not self.value:
-            return 'thirty miles'
-        if self.value == 'radius=50':
-            return 'fifty miles'
+    @classmethod
+    def append(cls):
+        return cls.MILES_30.get('value')
 
-    def __eq__(self, y):
-        return self.value == y.value
-
-    __repr__ = __str__
-
-    @staticmethod
-    def append():
-        print 'None'
-        return None
-
-    @staticmethod
-    def cmd_append():
+    @classmethod
+    def cmd_append(cls):
         choice = raw_input(
             '*****\nSearch surrounding...\n\n1 - {}\n2 - {}\n3 - {}\n4 - {}\n\n'
-                .format(Radius.MILES_5, Radius.MILES_10, Radius.MILES_30,
-                        Radius.MILES_50))
+                .format(cls.MILES_5.get('label'), cls.MILES_10.get('label'), cls.MILES_30.get('label'),
+                        cls.MILES_50.get('label')))
         if choice == '1':
-            print Radius.MILES_5
-            return Radius.MILES_5
+            print cls.MILES_5.get('value')
+            return cls.MILES_5.get('value')
         elif choice == '2':
-            print Radius.MILES_10
-            return Radius.MILES_10
+            print cls.MILES_10.get('value')
+            return cls.MILES_10.get('value')
         elif choice == '3':
-            print Radius.MILES_30
-            return Radius.MILES_30
+            print cls.MILES_30.get('value')
+            return cls.MILES_30.get('value')
         elif choice == '4':
-            print Radius.MILES_50
-            return Radius.MILES_50
+            print cls.MILES_50.get('value')
+            return cls.MILES_50.get('value')
         else:
             print '\nNot an option.  Defaulting to 30 miles.\n'
+            print cls.MILES_30.get('value')
+            return cls.MILES_30.get('value')
