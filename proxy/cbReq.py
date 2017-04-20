15eea0d8694a32df1816bebc74b969ed51fbe409
Nick Poloway <npoloway@catalystdevworks.com>
2017-04-20 16:23:40 -0400

    Now checks for pagination in query results

--- proxy/cbReq.py
+++ proxy/cbReq.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 
 from urlBuilder import UrlBuilder
 
+from scraper.cb.paginationHandler import PaginationHandler
+
 
 class CBReq(object):
     """Execute requests to CareerBuilder"""
@@ -19,8 +21,13 @@ class CBReq(object):
         zipcode = raw_input('Let\s scrape!  Query CareerBuilder.com for what zipcode?\n')
         search = raw_input('\nEnter any job description keywords to search separated by spaces:\n')
         x = UrlBuilder(zipcode, search)
-        print 'Querying CareerBuilder.com...'
-        page = urllib2.urlopen(x.build_url())
-        soup = BeautifulSoup(page, 'html.parser')
-        print 'Query complete'
-        return soup
+        # BEGIN EXTRACT TO SCRAPEHANDLER
+        # ex: scrapehandler.pullData(x.build_url())
+        soups = PaginationHandler.paginate(x.build_url())
+        print len(soups)
+        # print 'Querying CareerBuilder.com...'
+        # page = urllib2.urlopen(x.build_url())
+        # soup = BeautifulSoup(page, 'html.parser')
+        # print 'Query complete'
+        # END EXTRACT
+        # return soup
